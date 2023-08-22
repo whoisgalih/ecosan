765e7c37268b5d13033176184c164211618f403a
kyhchn <alvinkn.dev@gmail.com>
2023-08-22 13:57:17 +0700

    provide pick and update pp

--- lib/app/modules/home/edit/controllers/edit_controller.dart
+++ lib/app/modules/home/edit/controllers/edit_controller.dart
@@ -1,16 +1,19 @@
+import 'dart:io';
+
+import 'package:ecosan/app/constants/firebase_constants.dart';
 import 'package:ecosan/app/modules/home/controllers/home_controller.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class EditController extends GetxController {
-  //TODO: Implement EditController
   final homeController = HomeController.i;
-  final count = 0.obs;
   final nameEditingController = TextEditingController();
   final telpEditingController = TextEditingController();
   final addressEditingController = TextEditingController();
   final emailEditingController = TextEditingController();
   final passwordEditingController = TextEditingController();
+  FilePickerResult? result;
   @override
   void onInit() {
     super.onInit();
@@ -31,5 +34,49 @@ class EditController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void edit() async {
+    await homeController.authController.updateFirestoreUser();
+    Get.back();
+  }
+
+  Future<void> updateImage() async {
+    result = await FilePicker.platform.pickFiles(type: FileType.image);
+
+    if (result != null) {
+      File file = File(result!.files.single.path!);
+      final imageRef = storage.ref(
+          'images/${homeController.authController.firebaseUser.value!.uid}.${getFileExtensionFromPath(file.path)}');
+
+      try {
+        // Reference to Firebase Storage location
+
+        // Upload the selected image to Firebase Storage
+        await imageRef.putFile(file);
+
+        // Get the download URL of the uploaded image
+        final imageUrl = await imageRef.getDownloadURL();
+
+        // Update the user's image URL in Firestore
+        homeController.user.value.photoUrl = imageUrl;
+
+        await homeController.authController.updateFirestoreUser();
+        // Close the FilePicker dialog
+        Get.back();
+      } catch (e) {
+        // Handle any errors that occur during image upload or updating Firestore
+        print("Error uploading image: $e");
+        // You might want to show an error message to the user using Get.snackbar or other methods
+      }
+    }
+  }
+
+  String getFileExtensionFromPath(String filePath) {
+    if (filePath.isNotEmpty) {
+      int dotIndex = filePath.lastIndexOf('.');
+      if (dotIndex != -1 && dotIndex < filePath.length - 1) {
+        return filePath.substring(dotIndex + 1).toLowerCase();
+      }
+    }
+    return ''; // Return empty string if extension cannot be extracted
+  }
 }
--- lib/app/modules/home/faq/views/faq_view.dart
+++ lib/app/modules/home/faq/views/faq_view.dart
@@ -1,6 +1,10 @@
+import 'package:accordion/accordion.dart';
+import 'package:ecosan/app/modules/themes/colors.dart';
+import 'package:ecosan/app/modules/themes/fonts.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 import '../controllers/faq_controller.dart';
 
@@ -9,16 +13,62 @@ class FaqView extends GetView<FaqController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FaqView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'FaqView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () => Get.back(),
+              icon: const Icon(Icons.arrow_back, color: Colors.white)),
+          title: Text(
+            'FAQs',
+            style: TextStyles.header3.bold().copyWith(color: Colors.white),
+          ),
+          centerTitle: false,
+          backgroundColor: EcoSanColors.primary,
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: 2.5.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari Riwayat',
+                  suffixIcon: const Icon(Icons.search),
+                  fillColor: const Color(0xFFF1F1F1),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: EcoSanColors.primary),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 3.75.h,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tentang Sanitasi Air',
+                    style: TextStyles.normal.bold(color: EcoSanColors.primary),
+                  ),
+                  SizedBox(
+                    height: 2.5.h,
+                  ),
+                  Accordion(children: <AccordionSection>[
+                    AccordionSection(
+                        header: Text(
+                            'Apa saja service yang diberikan jika memesan perbaikan alat sanitasi air?'),
+                        content: Text(
+                            'Service yang diberikan berupa pengecekan alat, pencarian sumber masalah yang membuat alat tidak berfungsi, hingga penggantian alat apabila dirasa sudah tidak bisa di perbaiki.'))
+                  ])
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
