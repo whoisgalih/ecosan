11ae55b6898df22e47d4fce0e6d5c5f8e3d50f8e
Galih <galihakbar.ga91@gmail.com>
2023-08-29 13:40:30 +0700

    refactor: move widget to file

---
+++ lib/app/modules/widgets/icon_container.dart
@@ -0,0 +1,25 @@
+import 'package:ecosan/app/modules/themes/colors.dart';
+import 'package:flutter/material.dart';
+
+class IconContainer extends StatelessWidget {
+  const IconContainer({
+    super.key,
+    required this.icon,
+  });
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          color: EcoSanColors.secondary[50],
+          borderRadius: BorderRadius.circular(10)),
+      child: Icon(
+        icon,
+        color: EcoSanColors.secondary,
+      ),
+    );
+  }
+}
