a71969789318a1e1af350328eeb93f9a8007e39a
Galih <galihakbar.ga91@gmail.com>
2023-08-29 21:16:15 +0700

    remove unecessary imports

--- lib/app/modules/sampah/pickup-point/pickup-history/controllers/pickup_history_controller.dart
+++ lib/app/modules/sampah/pickup-point/pickup-history/controllers/pickup_history_controller.dart
@@ -1,6 +1,5 @@
 import 'package:ecosan/app/constants/loading_state.dart';
 import 'package:ecosan/app/models/trashHistory/trash_history_model.dart';
-import 'package:ecosan/app/modules/sampah/controllers/sampah_controller.dart';
 import 'package:ecosan/app/repository/trash_history/trash_history_repository.dart';
 import 'package:ecosan/app/routes/app_pages.dart';
 import 'package:get/get.dart';
