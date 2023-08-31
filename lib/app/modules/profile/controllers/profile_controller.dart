25557d6d681d13550a868b090b80ccb376b54ee6
kyhchn <alvinkn.dev@gmail.com>
2023-08-31 20:24:30 +0700

    add user repository

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,6 +1,7 @@
 import 'package:ecosan/app/models/user/user_model.dart';
 import 'package:ecosan/app/models/voucher/voucher.dart';
 import 'package:ecosan/app/modules/auth/controllers/auth_controller.dart';
+import 'package:ecosan/app/repository/user_repository.dart';
 import 'package:ecosan/app/repository/voucher_repository.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +9,7 @@ class ProfileController extends GetxController {
   AuthController authController = AuthController.authInstance;
   static ProfileController i = Get.find();
   VoucherRepository voucherRepository = VoucherRepository();
+  UserRepository userRepository = UserRepository();
   Rx<List<Voucher>> vouchers = Rx<List<Voucher>>([]);
   @override
   void onInit() {
